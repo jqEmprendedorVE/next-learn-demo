6fa0638d2a4e088d713fe527de7279a9e58d96b1
Julio Quintana <jquintana1801@gmail.com>
2019-04-17 17:23:53 -0300

    lesson 2 completed

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,7 +1,12 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-}
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default Index
